950fea66278796d4cd0b6bb6575077a71c560119
chnnick <nickchenschool@gmail.com>
2026-04-16 22:00:54 -0400

    redirect on 404 page

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,13 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
+const params = new URLSearchParams(window.location.search);
+const redirectPath = params.get('p');
+
+if (redirectPath) {
+  window.history.replaceState(null, '', redirectPath);
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
